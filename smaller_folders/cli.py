0eb9b29e9b06944fdf00db72c5cb9451471acd2a
Aatif Syed <aatif.syed@metaswitch.com>
2021-01-18 21:54:32 +0000

    Refactor config

--- smaller_folders/cli.py
+++ smaller_folders/cli.py
@@ -1,5 +1,8 @@
 import logging
 import argparse
+from pathlib import Path
+import dataclasses
+from typing import List
 
 logger = logging.getLogger(__name__)
 
@@ -7,41 +10,46 @@ logger = logging.getLogger(__name__)
 def main():
     logger.debug("Invoked at cli.py:main")
 
-    args = parse_arguments()
-    configure_logger(logger, args)
+    config = ArgumentParser().parse_args_to_config()
+    logger.debug(config)
 
-    logger.debug(args)
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        prog="smaller_folders",
-        # NOTE this must be kept in sync with `pyproject.toml`
-        description="Split an arbitrary number of files into sub-folders containing a specified number of files each.",
-    )
-    parser.add_argument(
-        "-l",
-        "--log-level",
-        choices=[
-            "debug",
-            "info",
-            "warning",
-            "error",
-            "critical",
-        ],
-        default="info",
-        help="set logging level for this tool",
-        type=str,
-    )
-    args = parser.parse_args()
-    return args
+    config.apply_to_logger(logger)
 
 
-def configure_logger(logger: logging.Logger, args: argparse.Namespace):
-    logger.setLevel(
-        level=getattr(
-            logging,
-            args.log_level.upper(),
+@dataclasses.dataclass
+class ProgramConfig:
+    log_level: str
+    files: List[Path] = dataclasses.field(default_factory=list)
+
+    def apply_to_logger(self, logger: logging.Logger):
+        logger.setLevel(
+            level=getattr(
+                logging,
+                self.log_level.upper(),
+            )
         )
-    )
-    logger.addHandler(logging.StreamHandler())
+        logger.addHandler(logging.StreamHandler())
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_argument(
+            "-l",
+            "--log-level",
+            choices=[
+                "debug",
+                "info",
+                "warning",
+                "error",
+                "critical",
+            ],
+            default="info",
+            help="set logging level for this tool",
+            type=str,
+        )
+        self.add_argument("files", nargs="*", type=Path)
+
+    def parse_args_to_config(self, *args, **kwargs) -> ProgramConfig:
+        args = self.parse_args(*args, **kwargs)
+        return ProgramConfig(log_level=args.log_level, files=args.files)
---
+++ tests/test_cli.py
@@ -0,0 +1,20 @@
+import logging as logger
+from pathlib import Path
+
+import pytest
+from smaller_folders.cli import ArgumentParser, ProgramConfig
+
+
+@pytest.fixture
+def argument_parser() -> ArgumentParser:
+    return ArgumentParser()
+
+
+def test_parse_many_files(argument_parser: ArgumentParser):
+    config = argument_parser.parse_args_to_config("a b c".split())
+    assert config.files == [Path("a"), Path("b"), Path("c")]
+
+
+def test_ignore_log_level(argument_parser: ArgumentParser):
+    config = argument_parser.parse_args_to_config("-l info a b c".split())
+    assert config.files == [Path("a"), Path("b"), Path("c")]
